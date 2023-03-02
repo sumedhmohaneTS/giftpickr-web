4b1ee82fcc1a84124ca69996c62ddbef0492a914
Sumedh Mohane <sumedhmohane@gmail.com>
2023-03-02 21:55:02 +0530

    minor changes

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/giftpickr-web'),
+      dir: require('path').join(__dirname, './coverage/giftpickr'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,17 +5,17 @@ import { EnvName } from '@enums/environment.enum';
 import packageInfo from '../../package.json';
 
 const scheme = 'http://';
-const host   = 'localhost';
-const port   = ':5000';
-const path   = '/api/';
+const host = 'localhost';
+const port = ':5000';
+const path = '/api/';
 
 const baseUrl = scheme + host + port + path;
 
 export const environment = {
-  production      : true,
-  version         : packageInfo.version,
-  appName         : 'EasyAngular',
-  envName         : EnvName.PROD,
-  defaultLanguage : 'en',
-  apiBaseUrl      : baseUrl,
+  production: true,
+  version: packageInfo.version,
+  appName: 'giftpickr',
+  envName: EnvName.PROD,
+  defaultLanguage: 'en',
+  apiBaseUrl: baseUrl,
 };
