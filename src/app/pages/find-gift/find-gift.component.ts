1e950ca767c10bc1e517a3714dc09471f27eff20
Sumedh Mohane <sumedhmohane@gmail.com>
2023-03-10 03:30:44 +0530

    minor changes in title

--- src/app/pages/find-gift/find-gift.component.ts
+++ src/app/pages/find-gift/find-gift.component.ts
@@ -1,6 +1,7 @@
 // Angular modules
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
+import { SeoService } from 'src/app/shared/seo/seo.service';
 
 @Component({
   selector: 'app-find-gift',
@@ -10,15 +11,20 @@ import { OnInit } from '@angular/core';
 export class FindGiftComponent implements OnInit {
 
   constructor
-    (
-  ) { }
+    (private seoService: SeoService
+    ) { }
 
   // -------------------------------------------------------------------------------
   // NOTE Init ---------------------------------------------------------------------
   // -------------------------------------------------------------------------------
 
   public ngOnInit(): void {
-
+    this.seoService.update({
+      title: 'Finding the Perfect gift for you',
+      description: `Let our AI-powered gift recommendations take the hassle out of picking gifts, and help you find the perfect gift that will bring a smile to your loved one's face.`,
+      url: window.location.href,
+      imageUrl: 'https://giftpickr-web.s3.ap-south-1.amazonaws.com/assets/GiftPickr-with-name.png'
+    });
   }
 
 }
